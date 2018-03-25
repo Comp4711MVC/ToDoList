a8553db14e3b3708c59152e0fe5d8e0c09ad0abd
RonnoTran <cbod.devil@gmail.com>
2018-03-25 14:33:25 -0700

    Fixed load and store method

--- application/models/Tasks.php
+++ application/models/Tasks.php
@@ -7,6 +7,62 @@ class Tasks extends XML_Model
         parent::__construct(APPPATH . '../data/tasks.xml', 'id');
     }
 
+    protected function load()
+    {
+        if (($tasks = simplexml_load_file($this->_origin)) !== FALSE)
+        {
+            foreach ($tasks as $task) {
+                $record = new stdClass();
+                $record->id = (int) $task->id;
+                $record->task = (string) $task->task    ;
+                $record->priority = (int) $task->priority;
+                $record->size = (int) $task->size;
+                $record->group = (int) $task->group;
+                $record->deadline = (string) $task->deadline;
+                $record->status = (int) $task->status;
+                $record->flag = (int) $task->flag;
+                $this->_data[$record->id] = $record;
+            }
+        }
+        $this->reindex();
+        parent::load();
+    }
+
+    protected function store()
+    {
+        /*
+        // rebuild the keys table
+        $this->reindex();
+        //---------------------
+        */
+        if (($handle = fopen($this->_origin, "w")) !== FALSE)
+        {
+                fputcsv($handle, $this->_fields);
+                foreach ($this->_data as $key => $record)
+                    fputcsv($handle, array_values((array) $record));
+                fclose($handle);
+            // --------------------
+            $xmlDoc = new DOMDocument( "1.0");
+            $xmlDoc->preserveWhiteSpace = false;
+            $xmlDoc->formatOutput = true;
+            $data = $xmlDoc->createElement($this->xml->getName());
+            foreach($this->_data as $key => $value)
+            {
+                $task  = $xmlDoc->createElement($this->xml->children()->getName());
+                foreach ($value as $itemkey => $record ) {
+                    $item = $xmlDoc->createElement($itemkey, htmlspecialchars($record));
+                    $task->appendChild($item);
+                }
+                $data->appendChild($task);
+            }
+            $xmlDoc->appendChild($data);
+            $xmlDoc->saveXML($xmlDoc);
+            $xmlDoc->save($this->_origin);
+        }
+    }
+
+
+
     function getCategorizedTasks()
     {
         // extract the undone tasks
@@ -44,13 +100,16 @@ class Tasks extends XML_Model
 
 }
 
-// return -1, 0, or 1 of $a's category name is earlier, equal to, or later than $b's
-function orderByCategory($a, $b)
-{
-    if ($a->group < $b->group)
-        return -1;
-    elseif ($a->group > $b->group)
-        return 1;
-    else
-        return 0;
-}
+    // return -1, 0, or 1 of $a's category name is earlier, equal to, or later than $b's
+    function orderByCategory($a, $b)
+    {
+        if ($a->group < $b->group)
+            return -1;
+        elseif ($a->group > $b->group)
+            return 1;
+        else
+            return 0;
+    }
+
+
+
